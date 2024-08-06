9080be10adc5e67f27c81ff776cd171316d21f86
KrzucH <krzynowak93@gmail.com>
2024-08-06 13:12:38 +0200

    Some Refactor ver2

--- homework/advanced-calculator/advancedCalculator.hpp
+++ homework/advanced-calculator/advancedCalculator.hpp
@@ -1,21 +1,9 @@
-#include <iostream>
 #include <algorithm>
-#include <map>
-#include <functional>
 #include <cmath>
+#include <functional>
+#include <iostream>
+#include <map>
 #include <string>
-// Wszystkie komendy kalkulatora powinny być przechowywane w mapie, która jako klucz przechowuje znak `char` odwołujący się do konkretnej komendy (np. `+` -> dodaj , `%` -> modulo), a jako wartości `std::function<>` będące wraperem na wyrażenia lambda dokonujące określonej kalkulacji.
-
-// Program powinien także zwracać odpowiedni kod błędu, jeżeli użytkownik poda złe dane. Np. dzielenie przez `0` lub spróbuje dodać `ala + 5`.
-
-// * Input: `5 + 5` -> operacja dodawania dwóch liczb `5` i `5` -> output: `10`.
-// * Input: `5 ^ 2` -> operacje potęgowania -> output `25`.
-// * Input: `125 $ 3` -> operacja pierwiastka (sqrt za długie), pierwiastek sześcienny z `125` -> output: `5`.
-
-// Zadanie dostarcz na gałąź [advanced-calculator](https://github.com/coders-school/stl/tree/advanced-calculator/homework/advanced-calculator).
-
-// ___
-
 
 enum class ErrorCode {
     OK,
@@ -26,8 +14,4 @@ enum class ErrorCode {
     ModuleOfNonIntegerValue
 };
 
-
-ErrorCode process (std::string, double*);
-
-
-
+ErrorCode process(std::string, double*);
--- homework/advanced-calculator/main.cpp
+++ homework/advanced-calculator/main.cpp
@@ -1,19 +1,5 @@
 #include "advancedCalculator.hpp"
 
 int main() {
-    
-    int num = 11;
-
-
-
-std::cout << std::tgamma(num); 
-
-//    int num1 = num[1] - '0';
-
-//    for (auto i = 0; i<= num.size() - 1; i++) {
-//     if(num[i] - '0' <= 0 && num[i] - '0' >= 2)
-//     std::cout << num[i] - '0';
-//    }
-
     return 0;
 }
